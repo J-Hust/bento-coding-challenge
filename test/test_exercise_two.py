9063cbe576392d520ca3f6d3f3cc4c80e07669fd
J-Hust <jhustrulid@gmail.com>
2020-10-25 10:46:30 -0500

    working solution first draft

--- test/test_exercise_two.py
+++ test/test_exercise_two.py
@@ -1,6 +1,25 @@
 import unittest
+from src.two import count_recurring
+
+class TestExerciseTwo(unittest.TestCase):
+
+    def test_no_repeating(self):
+        result = count_recurring('abcd')
+        self.assertEqual(result, 'abcd')
+
+    def test_one_reapeating(self):
+        result = count_recurring('abbbcd')
+        self.assertEqual(result, 'ab3cd')
+
+    def test_repeating_at_end(self):
+        result = count_recurring('abbbcddd')
+        self.assertEqual(result, 'ab3cd3')
+
+    def test_all_recurring(self):
+        result = count_recurring('rrdd')
+        self.assertEqual(result, 'r2d2')
 
 
-Class TestExerciseTwo(unittest.TestCase):
-
+if __name__ == '__main__':
+    unittest.main()
 
